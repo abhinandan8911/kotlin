8c8211e03839616a81031bd3b18ec69df9baf2e5
Abhinandan Bhattacharya <abhinandan.bhattacharya@gmail.com>
2022-02-17 16:35:21 -0600

    LeetCode - https://leetcode.com/explore/interview/card/bloomberg/68/array-and-strings/2939/

---
+++ src/main/kotlin/leetcode/Anagram.kt
@@ -0,0 +1,9 @@
+package leetcode
+
+class Anagram {
+    fun isAnagram(s: String, t: String): Boolean {
+        val sSorted = s.toCharArray().sorted()
+        val tSorted = t.toCharArray().sorted()
+        return sSorted == tSorted
+    }
+}
