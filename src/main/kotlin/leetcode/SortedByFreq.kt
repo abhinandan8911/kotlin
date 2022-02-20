9f3524ab10cbade8f3a1856cea7dfae245a61fb3
Abhinandan Bhattacharya <abhinandan.bhattacharya@gmail.com>
2022-02-20 15:25:57 -0600

    LeetCode - https://leetcode.com/explore/interview/card/bloomberg/72/sorting-and-searching/432/

---
+++ src/main/kotlin/leetcode/SortedByFreq.kt
@@ -0,0 +1,24 @@
+package leetcode
+
+class SortedByFreq {
+    fun frequencySort(s: String): String {
+        val mapChars = mutableMapOf<Char, Int>()
+        for(ch in s.toCharArray()) {
+            mapChars[ch] = mapChars.getOrDefault(ch, 0) + 1
+        }
+        val entries = mapChars.entries.asSequence()
+            .sortedByDescending { it.value }
+            .toList()
+        val strBuilder = StringBuilder()
+        for (entry in entries) {
+            for (i in 0 until entry.value) {
+                strBuilder.append(entry.key)
+            }
+        }
+        return strBuilder.toString()
+    }
+}
+
+fun main() {
+    println(SortedByFreq().frequencySort("tree"))
+}
