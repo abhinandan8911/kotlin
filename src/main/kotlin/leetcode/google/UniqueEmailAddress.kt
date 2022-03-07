af1aee19d6d49f922b2bb4819a4ab16446b1e3aa
Abhinandan Bhattacharya <abhinandan.bhattacharya@gmail.com>
2022-03-07 14:35:02 -0600

    LeetCode - https://leetcode.com/explore/interview/card/google/67/sql-2/3044/

---
+++ src/main/kotlin/leetcode/google/UniqueEmailAddress.kt
@@ -0,0 +1,22 @@
+package leetcode.google
+
+// https://leetcode.com/explore/interview/card/google/67/sql-2/3044/
+class UniqueEmailAddress {
+    fun numUniqueEmails(emails: Array<String>): Int {
+        val emailSet = mutableSetOf<String>()
+        for (email in emails) {
+            val emailPart = email.split("@")
+            val local = emailPart.get(0)
+            val domain = emailPart.get(1)
+            var parsedLocal = local.replace(".", "")
+            parsedLocal = parsedLocal.substringBefore('+')
+            emailSet.add("$parsedLocal@$domain")
+        }
+        return emailSet.size
+    }
+}
+
+fun main() {
+    val strings = arrayOf("test.email+alex@leetcode.com","test.e.mail+bob.cathy@leetcode.com","testemail+david@lee.tcode.com")
+    println(UniqueEmailAddress().numUniqueEmails(strings))
+}
